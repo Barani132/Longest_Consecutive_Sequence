d6ad58e88bac2ec6b61e56be570fb2345df5d95e
Barani132 <barani.b2137@gmail.com>
2026-04-10 22:41:42 +0530

    Implement longest consecutive sequence algorithm

---
+++ long.py
@@ -0,0 +1,14 @@
+#Longest Consecutive Sequence
+arr = [100,4,200,1,3,2]
+
+nums = set(arr)
+longest = 0
+
+for num in nums:
+    if num - 1 not in nums:
+        length = 1
+        while num + length in nums:
+            length += 1
+        longest = max(longest, length)
+
+print("Longest:", longest)
